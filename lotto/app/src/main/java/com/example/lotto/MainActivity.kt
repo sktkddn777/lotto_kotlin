358301caafdcc92d41752815fb59856e684431ac
sktkddn777 <37575974+sktkddn777@users.noreply.github.com>
2021-02-28 00:28:07 +0900

    add vibrator function

--- lotto/app/src/main/java/com/example/lotto/MainActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.lotto
 
+import android.content.Context
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.CountDownTimer
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.example.lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,16 +21,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         val ball_list = listOf<TextView>(binding.num1, binding.num2,binding.num3, binding.num4,binding.num5, binding.num6)
-
-
         binding.click.setOnClickListener(){
 
             binding.lotteryWhirl.playAnimation()
             val countDownTimer = object : CountDownTimer(5000, 100){
-
                 override fun onFinish() {
                 }
-
                 override fun onTick(millisUntilFinished: Long) {
                     val lottery_list = mutableListOf<Int>()
                     while (lottery_list.size <= 6){
@@ -42,6 +40,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             countDownTimer.start()
+            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            val vibrationEffect = VibrationEffect.createOneShot(3000,100)
+            vibrator.vibrate(vibrationEffect)
         }
     }
 }
